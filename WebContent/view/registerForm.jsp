4e70ab5905017a6c4e63b2bddc7c255d67e35ea6
youseoungjo <sjyou5381@naver.com>
2020-09-26 18:49:57 +0900

    create hw1-helloMVC project

---
+++ WebContent/view/registerForm.jsp
@@ -0,0 +1,53 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Registration Page</title>
+</head>
+<style>
+div.ex {
+	text-align: center;
+	width: 18%;
+	padding: 10px;
+	border: 5px solid grey;
+	margin: 0px
+}
+</style>
+<body>
+	<h1>Registration Form</h1>
+	<div class="ex">
+		<h3>Enter Information Here</h3>
+		<form action="/hw1-helloMVC/doRegister" method="post">
+			<table cellpadding="5">
+				<tr>
+					<td>아이디</td>
+					<td><input type="text" name="id" /></td>
+				</tr>
+				<tr>
+					<td>비밀번호</td>
+					<td><input type="password" name="password" /></td>
+				</tr>
+				<tr>
+					<td>이름</td>
+					<td><input type="text" name="name" /></td>
+				</tr>
+				<tr>
+					<td>성별</td>
+					<td><input type="radio" name="gender" value="male" />남자
+					<input type="radio" name="gender" value="female"/>여자</td>
+				</tr>
+				<tr>
+					<td>이메일</td>
+					<td><input type="email" name="email" /></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" value="register" /></td>
+				</tr>
+			</table>
+		</form>
+	</div>
+</body>
+</html>
---
+++ src/controller/DoRegister.java
@@ -0,0 +1,54 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Customer;
+import service.CustomerService;
+
+/**
+ * Servlet implementation class DoRegister
+ */
+@WebServlet("/doRegister")
+public class DoRegister extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DoRegister() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		String name = request.getParameter("name");
+		String gender = request.getParameter("gender");
+		String email = request.getParameter("email");
+		
+		CustomerService service = (CustomerService) CustomerService.getInstance();
+		Customer customer = new Customer(id, password, name, gender, email);
+		service.addCustomer(customer);
+		
+		String page;
+		
+		page ="/view/registerSuccess.jsp";
+		request.setAttribute("customer", customer);
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(page);
+		dispatcher.forward(request, response);
+	}
+
+}
